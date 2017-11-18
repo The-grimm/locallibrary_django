cc81c6d124bf0ad6604e31560c9fe2fd9724657d
sriharsha <The-grimm@users.noreply.github.com>
2017-11-18 15:37:18 +0530

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def home(request):
     return HttpResponse("<h2>HEY!</h2>")
 # Create your views here.
